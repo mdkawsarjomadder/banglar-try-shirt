b28eb74c4eea29d28118d212fd595d1a54b5257c
Your Name <you@example.com>
2023-06-11 01:57:33 +0600

    inistial data loader

---
+++ src/Components/OrderReview/OrderReview.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderReview = () => {
+      return (
+            <nav>
+                  <h2>Oder-Review Page</h2>
+            </nav>
+      );
+};
+
+export default OrderReview;
